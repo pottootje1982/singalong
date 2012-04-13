90df01132d18404c1af76d3d0b73e56f9018483d
wouterpot2 <wouterpot2@84cfc1e2-b108-bf3c-5ef4-fd60c665c2f2>
2012-04-13 08:51:54 +0000

    - created separate files for linux & windows implementations of system library functions

--- source/singalong/lua_system.h
+++ source/singalong/lua_system.h
@@ -2,4 +2,10 @@
 
 #include "global.h"
 
+int lua_sleep(lua_State *L);
+int lua_setIcon(lua_State *L);
+int lua_shellExecute(lua_State *L);
+int lua_shellExecuteWait(lua_State *L);
+int lua_createProcess(lua_State *L);
+
 int luaopen_system(lua_State *L);
