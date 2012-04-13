f929bb76e1bb0088472e7adb1c73ab797a7c812c
wouterpot2 <wouterpot2@84cfc1e2-b108-bf3c-5ef4-fd60c665c2f2>
2012-04-13 08:29:53 +0000

    - replaced some functions

--- source/singalong/lua_system.h
+++ source/singalong/lua_system.h
@@ -1,10 +1,5 @@
 #pragma once
 
 #include "global.h"
-#include "lua_system.h"
 
-int lua_sleep(lua_State *L);
-int lua_setIcon(lua_State *L);
-int lua_shellExecute(lua_State *L);
-int lua_shellExecuteWait(lua_State *L);
-int lua_createProcess(lua_State *L);
+int luaopen_system(lua_State *L);
--- source/singalong/singalong.cpp
+++ source/singalong/singalong.cpp
@@ -35,34 +35,6 @@ extern "C" {
 
 #include <conio.h>
 #include <string>
-#include <vector>
-#include <algorithm>
-
-template <class T> float edit_distance(const T& s1, const T& s2)
-{
-  const size_t len1 = s1.size(), len2 = s2.size();
-  std::vector<std::vector<unsigned int> > d(len1 + 1, std::vector<unsigned int>(len2 + 1));
-
-  d[0][0] = 0;
-  for(unsigned int i = 1; i <= len1; ++i) d[i][0] = i;
-  for(unsigned int i = 1; i <= len2; ++i) d[0][i] = i;
-
-  for(unsigned int i = 1; i <= len1; ++i)
-    for(unsigned int j = 1; j <= len2; ++j)
-
-      d[i][j] = std::min( std::min(d[i - 1][j] + 1,d[i][j - 1] + 1),
-      d[i - 1][j - 1] + (s1[i - 1] == s2[j - 1] ? 0 : 1) );
-  return 1-d[len1][len2]/(float)(std::max(len1, len2));
-}
-
-static int lua_levenshtein(lua_State *L)
-{
-  std::string str1 = lua_tostring(L, 1);
-  std::string str2 = lua_tostring(L, 2);
-  float res = edit_distance(str1, str2);
-  lua_pushnumber(L, res);
-  return 1;
-}
 
 static int traceback (lua_State *L) {
   if (!lua_isstring(L, 1))  /* 'message' not a string? */
@@ -95,16 +67,6 @@ static int docall (lua_State *L, int narg, int clear) {
   if (status != 0) lua_gc(L, LUA_GCCOLLECT, 0);
   return status;
 }
-
-static luaL_Reg system_func[] = {
-    {"levenshtein",       lua_levenshtein},
-    {"sleep",             lua_sleep},
-    {"setIcon",           lua_setIcon},
-    {"createProcess",     lua_createProcess},
-    {"shellExecute",      lua_shellExecute},
-    {"shellExecuteWait",  lua_shellExecuteWait},
-    {NULL,        NULL}
-};
 
 void close( lua_State * L ) 
 {
@@ -142,8 +104,7 @@ int main(int argc, char ** argv)
   pdfdoc_register(L);
   pdfpage_register(L);
   clipboard_register(L);
-
-  luaL_register(L, "system", system_func);
+  luaopen_system(L);
 
 #if _DEBUG 
   lua_pushboolean(L, true);
