52735d0488e90a504d4f774cf76339594744c740
wouterpot2@gmail.com <wouterpot2@gmail.com@84cfc1e2-b108-bf3c-5ef4-fd60c665c2f2>
2012-05-22 14:55:48 +0000

    - run tests when building

--- trunk/bin/tests/run_tests.lua
+++ trunk/bin/tests/run_tests.lua
@@ -1,9 +1,20 @@
+require 'misc'
+
+local args = {...}
+
+local testDir = '.'
+if args[1] then
+  testDir = args[1]
+  package.path = package.path .. ';' .. testDir .. '\\?.lua'
+end
+
 package.path = package.path .. ';..\\?.lua'
 
 require 'luaunit'
 require 'lfs'
 
-for file in lfs.dir('.') do
+---[[
+for file in lfs.dir(testDir) do
   if file:match('lua$') and not file:match('run_tests') then
     local fileWithoutExt = file:match('(.*)%.lua$')
     print(fileWithoutExt)
@@ -12,3 +23,4 @@ for file in lfs.dir('.') do
 end 
 
 LuaUnit:run()
+--]]
