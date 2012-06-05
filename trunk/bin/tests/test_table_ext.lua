9ae1602913f2eb2da24fdfa0cc67d767d0099490
wouterpot2 <wouterpot2@84cfc1e2-b108-bf3c-5ef4-fd60c665c2f2>
2012-06-05 21:11:14 +0000

    - implemented feature to remove doubles from playlist

--- trunk/bin/tests/test_table_ext.lua
+++ trunk/bin/tests/test_table_ext.lua
@@ -1,5 +1,6 @@
 require 'misc'
 require 'luaunit'
+require 'title_bar_gui'
 
 TestTableExt = {}
 
@@ -10,4 +11,41 @@ function TestTableExt:testSaveTable()
   --assert(res)
 end
 
-TestTableExt:testSaveTable()
+function TestTableExt:testTableFind()
+  assertEquals(table.ifind({1,4,6,7}, 6), 3)
+  assertEquals(table.ifind({1,4,6,7}, function(i, elem) return elem > 5 end), 3)
+  assertEquals(table.ifind({1,4,6,7}, function(i, elem) return elem > 8 end), nil)
+
+  assertEquals(table.ifind({1,4,6,7}, function(i, elem) return elem < 4 end, 1), 1)
+  assertEquals(table.ifind({1,4,6,7}, function(i, elem) return elem < 4 end, 3), nil)
+  assertEquals(table.ifind({1,4,6,7}, function(i, elem) return elem <= 6 end, 3), 3)
+
+
+  assertEquals(table.find({a=3, b=4, c=7}, 3), 'a')
+  assertEquals(table.find({a=3, b=4, c=7}, function(key, elem) return elem > 5 end), 'c')
+  assertEquals(table.find({a=3, b=4, c=7}, function(key, elem) return elem > 8 end), nil)
+end
+
+function TestTableExt:testTableRemoveDoubles()
+  local unique = table.removeDoubles({1,3,4,4,5,5,6})
+  assert(table.equals(unique, {1,3,4,5,6}))
+
+  local tableDoubles = {
+    {artist = 'beatles', title = 'hey jude'},
+    {artist = 'beatles', title = 'hey jude'},
+    {artist = 'neil young', title = 'unknown legend'},
+    {artist = 'beatles', title = 'hey jude'},
+    {artist = 'beatles', title = 'hey jude'},
+    {artist = 'neil young', title = 'king'}
+  }
+  unique = table.removeDoubles(tableDoubles, title_bar_gui.compareTracks)
+  assert(table.equals(unique,
+  {
+    {artist = 'beatles', title = 'hey jude'},
+    {artist = 'neil young', title = 'unknown legend'},
+    {artist = 'neil young', title = 'king'}
+  }
+    ))
+end
+
+TestTableExt:testTableRemoveDoubles()
