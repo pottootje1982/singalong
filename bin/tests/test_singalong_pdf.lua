7954435a1be13adf0844aed9df1525490bd1251a
wouterpot2@gmail.com <wouterpot2@gmail.com@84cfc1e2-b108-bf3c-5ef4-fd60c665c2f2>
2012-06-06 13:51:33 +0000

    - moved test lyrics to testdata

--- bin/tests/test_singalong_pdf.lua
+++ bin/tests/test_singalong_pdf.lua
@@ -4,16 +4,20 @@ require 'playlist_api'
 require 'singalongpdf'
 require 'test_setup'
 
-_TestSingalongPdf = {}
-
-function _TestSingalongPdf:testGenerateSongbook()
+function testGenerateSongbook()
   local tracks = playlist_api.gatherMp3Info(testDataDir('verukkelijke 715 zang.m3u'))
   tracks = {tracks[1], tracks[2], tracks[3], tracks[4]}
   local searchSite = search_sites[1]
   for i, track in ipairs(tracks) do
     local fn = os.format_bare_file(track.artist, track.title, 'txt')
-    fn = F(system.getExecutablePath(), 'tests', fn)
-    cache.addToCache(track, searchSite, os.format_file('txt', searchSite, track), os.read(fn))
+    fn = testDataDir(fn)
+    local content = os.read(fn)
+    assert(content, 'File not found!')
+    cache.addToCache(track, searchSite, os.format_file('txt', searchSite, track), content)
   end
   getPdfGenerator().generateSongbook(tracks, 'testGenerateSongbook')
+end
+
+if not RUN_UNIT_TESTS then
+  testGenerateSongbook()
 end
