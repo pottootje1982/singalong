fbaa421572f7cf26d611423634c99108e4d5dff0
wouterpot2@gmail.com <wouterpot2@gmail.com@84cfc1e2-b108-bf3c-5ef4-fd60c665c2f2>
2012-06-06 13:38:30 +0000

    - moved song.mp3 and fixed test_id3.lua

--- trunk/bin/tests/test_id3.lua
+++ trunk/bin/tests/test_id3.lua
@@ -1,10 +1,12 @@
 require 'misc'
 require 'id3'
+require 'test_setup'
 require 'luaunit'
 
 TestId3 = {}
 
 -- TODO: fix table.unmarshal so that it can load empty tables
 function TestId3:testReadAlbum()
-  table.print(id3.readtags('song.mp3'))
+  local tags = id3.readtags(testDataDir('song.mp3'))
+  assertEquals(tags.album, 'None')
 end
