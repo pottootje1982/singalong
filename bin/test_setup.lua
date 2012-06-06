90e80cb5f61cc29ed74c9f7a929e3faabe82c6ca
wouterpot2@gmail.com <wouterpot2@gmail.com@84cfc1e2-b108-bf3c-5ef4-fd60c665c2f2>
2012-06-06 13:38:30 +0000

    - moved song.mp3 and fixed test_id3.lua

--- bin/test_setup.lua
+++ bin/test_setup.lua
@@ -4,5 +4,9 @@ local executablePath = system.getExecutablePath()
 TEST_PATH = F(executablePath, 'tests')
 package.path = package.path .. ';' .. TEST_PATH .. '\\?.lua'
 
+function testDataDir(file)
+  return F(TEST_PATH, 'testdata', file)
+end
+
 require 'load_config'(TEST_PATH)
 require 'lfs'
